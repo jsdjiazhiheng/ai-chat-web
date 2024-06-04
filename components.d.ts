a26cbccd787c834adab5da8a2318d62d60a54726
JiaZH <1412148237@qq.com>
2024-06-04 18:15:23 +0800

    feat(对话): 增加预览图片

--- components.d.ts
+++ components.d.ts
@@ -8,6 +8,7 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
+    ImagePreview: typeof import('./src/components/ImagePreview.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }
---
+++ src/components/ImagePreview.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+const props = defineProps({
+  imageSrc: {
+    type: String,
+    required: true,
+    default: null,
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+    default: null,
+  },
+})
+
+const showPreview = ref(false)
+</script>
+
+<template>
+  <div class="relative w-full">
+    <img
+      :src="thumbnail"
+      alt="Thumbnail"
+      class="cursor-pointer transition duration-300 ease-in-out rounded"
+      @click="showPreview = true"
+    />
+  </div>
+
+  <div
+    v-if="showPreview"
+    class="absolute inset-0 flex items-center justify-center p-16 bg-opacity-50 bg-black z-50"
+    @click="showPreview = false"
+  >
+    <img
+      :src="imageSrc"
+      alt="Preview"
+      class="max-h-full max-w-full rounded"
+    />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
